07e39e447341085a66c1a6da1c8a0c2e16ae2506
tvchung <hna.tvchung@gmmail.com>
2026-08-11 15:51:36 +0700

    Update Ex01

---
+++ Devmaster.Internship.CSharp2608/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Devmaster.Internship.CSharp2608
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex01.OrderCalculator/Enums/ShippingMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01.OrderCalculator.Enums
+{
+    public  enum ShippingMethod
+    {
+        Standard = 1, // Giao hàng tiêu chuẩn
+        Express = 2,  // Giao hàng nhanh
+        Fast = 3      // Giao hàng hỏa tốc
+    }
+}
